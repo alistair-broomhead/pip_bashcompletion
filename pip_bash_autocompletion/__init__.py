410d38b058fb30164bacf585912f87461f084ec3
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-02-11 11:47:50 +0000

    Fix package cache and auto-add completion to bash profile

--- pip_bash_autocompletion/__init__.py
+++ pip_bash_autocompletion/__init__.py
@@ -1,6 +1,14 @@
 import sys
 import os
 
+BASH_COMPLETION_DIR = os.path.expanduser('~/bash_completion.d')
+BASH_COMPLETION_SOURCE = """
+if [ -d {0} ]; then
+    . {0}/*
+fi
+""".format(BASH_COMPLETION_DIR)
+
+
 try:
     import xmlrpclib
 except ImportError:
@@ -24,19 +32,37 @@ def update():
     sys.stderr.write("\nConnecting to pypi...\n")
     client = xmlrpclib.ServerProxy('https://pypi.python.org/pypi')
     index_file_name = os.path.expanduser('~/.pip_index')
+
     sys.stderr.write("Opening index file...\n")
     with open(index_file_name, 'w') as index_file:
         sys.stderr.write("Retrieving package list...\n")
         packages = client.list_packages()
-        index_file.writelines(packages)
-    sys.stderr.write("Done!\n")
+        sys.stderr.write("Writing package cache...\n")
+        index_file.write('\n'.join(packages))
+    sys.stderr.write("Updated cache!\n")
 
 
 def install():
-    destination_dir = os.path.expanduser('~/bash_completion.d')
+    sys.stderr.write("\n")
 
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
+    if not os.path.exists(BASH_COMPLETION_DIR):
+        sys.stderr.write("Creating {0}...\n".format(BASH_COMPLETION_DIR))
+        os.makedirs(BASH_COMPLETION_DIR)
 
-    destination_name = os.path.join(destination_dir, 'pip')
+    destination_name = os.path.join(BASH_COMPLETION_DIR, 'pip')
+    sys.stderr.write("Writing {0}...\n".format(destination_name))
     open(destination_name, 'w').write(AUTOCOMPLETE)
+
+    sys.stderr.write("Looking for entry in ~/.bashrc...\n")
+    profile_file_name = os.path.expanduser('~/.bashrc')
+    with open(profile_file_name) as profile_file:
+        installed = (BASH_COMPLETION_SOURCE in profile_file.read())
+
+    if installed:
+        sys.stderr.write("Found entry in in ~/.bashrc...\n")
+    else:
+        with open(profile_file_name, 'a') as profile_file:
+            sys.stderr.write("Appending entry to ~/.bashrc...\n")
+            profile_file.write(BASH_COMPLETION_SOURCE)
+
+    sys.stderr.write("Installed bash completion!\n")
